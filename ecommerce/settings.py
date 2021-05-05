027a2980c7cab8f93a23d92a6adab3c1190b1126
Prince Bubezi <72832457+Blacking-highness@users.noreply.github.com>
2021-05-05 23:31:13 +0300

    .

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -146,3 +149,8 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/bye"
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
