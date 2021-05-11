986475be3ff4b9cc346f651a488f766f7a7b8f4b
Prince Bubezi <72832457+Blacking-highness@users.noreply.github.com>
2021-05-11 17:20:55 +0300

    Media Folder Change
    Changed location of Media Folder to BASE from static

--- ecommerce/settings/dev.py
+++ ecommerce/settings/dev.py
@@ -1,11 +1,13 @@
 from .base import *
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images/media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
